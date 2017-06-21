e06d4d4b213d5abd92d809db64204c2fd9d9107a
Covington Doan <github.covington@mailhero.io>
2017-06-21 14:31:05 -0500

    Close #13
    1. I found myself adding `mt0` a lot. Is there a better approach?
    2. The styles output with content created from the CMS need to be reviewed.
    3. I had to add a custom stylesheet to match the form styles.

--- src/js/cms-preview-templates/post.js
+++ src/js/cms-preview-templates/post.js
@@ -6,14 +6,14 @@ export default class PostPreview extends React.Component {
     const {entry, widgetFor} = this.props;
 
     return <div className="mw6 center ph3 pv4">
-      <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
+      <h1 className="f2 lh-title b mt0 mb3">{ entry.getIn(["data", "title"])}</h1>
       <div className="flex justify-between grey-3">
         <div style={{
           width: "80px",
           height: "80px"
         }}></div>
-        <p>{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
-        <p>Read in x minutes</p>
+        <p className="mt0">{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
+        <p className="mt0">Read in x minutes</p>
       </div>
       <div className="cms mw6">
         { widgetFor("body") }
--- src/js/cms-preview-templates/products.js
+++ src/js/cms-preview-templates/products.js
@@ -18,7 +18,7 @@ export default class PostPreview extends React.Component {
         <div className="mw7 center ph3">
           <div className="db mb3">
             <div className="mw7 relative bg-fix-primary mb3">
-              <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary">
+              <h1 className="f2 f1-l b di lh-title mt0 mb3 white mw6 bg-primary">
                 { entry.getIn(["data", "title"]) }
               </h1>
             </div>
@@ -30,13 +30,13 @@ export default class PostPreview extends React.Component {
 
       <div className="bg-off-white pv4">
         <div className="ph3 mw7 center">
-          <h2 className="f2 b lh-title mb2">{entry.getIn(["data", "intro", "heading"])}</h2>
-          <p className="mb4 mw6">{entry.getIn(["data", "intro", "description"])}</p>
+          <h2 className="f2 b lh-title mt0 mb2">{entry.getIn(["data", "intro", "heading"])}</h2>
+          <p className="mt0 mb4 mw6">{entry.getIn(["data", "intro", "description"])}</p>
 
           <div className="flex-ns flex-wrap mhn2-ns mb3">
             {(entry.getIn(["data","intro", "blurbs"]) || []).map((blurb, index) => <div className="ph2-ns w-50-ns mb4" key={index}>
               <img src={blurb.get("image") && getAsset(blurb.get("image"))} alt="" className="center db mb3" style={{width: "240px"}}/>
-              <p>{blurb.get("text")}</p>
+              <p className="mt0">{blurb.get("text")}</p>
             </div>)}
           </div>
         </div>
@@ -44,8 +44,8 @@ export default class PostPreview extends React.Component {
 
       <div className="mw7 center">
         <div className="mw6 ph3 mb3">
-          <h3 className="f3 b lh-title mb2">{entry.getIn(["data", "main", "heading"])}</h3>
-          <p>{entry.getIn(["data", "main", "description"])}</p>
+          <h3 className="f3 b lh-title mt0 mb2">{entry.getIn(["data", "main", "heading"])}</h3>
+          <p className="mt0">{entry.getIn(["data", "main", "description"])}</p>
         </div>
       </div>
 
@@ -69,7 +69,7 @@ export default class PostPreview extends React.Component {
       <div className="pb4">
         {(entry.getIn(['data', 'testimonials']) || []).map((testimonial, index) => <div className="center mb3 ph3" key={index}>
         	<blockquote className="bg-grey-1 primary pa3 mb3 br1 b mw6 center">
-        		<p className="f4 mb0">“{testimonial.get('quote')}”</p>
+        		<p className="f4 mv0">“{testimonial.get('quote')}”</p>
         		<cite className="tr db grey-3">{testimonial.get('author')}</cite>
         	</blockquote>
         </div>)}
@@ -80,24 +80,24 @@ export default class PostPreview extends React.Component {
       <div className="bg-off-white pv4 ph3">
       	<div className="mw7 center">
 
-      		<h2 className="f2 b lh-title mb3">{entry.getIn(['data', 'pricing', 'heading'])}</h2>
-      		<p className="mw6">{entry.getIn(['data', 'pricing', 'description'])}</p>
+      		<h2 className="f2 b lh-title mt0 mb3">{entry.getIn(['data', 'pricing', 'heading'])}</h2>
+      		<p className="mt0 mw6">{entry.getIn(['data', 'pricing', 'description'])}</p>
 
       		<div className="flex-ns mhn2-ns mw7">
             {(entry.getIn(['data', 'pricing', 'plans']) || []).map((plan, index) => <div className="w-33-ns ph2" key={index}>
               <div className="ph2">
 
-              	<h3 className="b f5 grey-3 tc lh-title mb3">{plan.get('plan')}</h3>
+              	<h3 className="b f5 grey-3 tc lh-title mt0 mb3">{plan.get('plan')}</h3>
 
-              	<p className="primary f1 b tc lh-title center">
+              	<p className="primary f1 b tc lh-title mt0 center">
               		<span className="f4">$</span>{plan.get('price')}
               	</p>
 
--              	<p className="b">{plan.get('description')}</p>
+-              	<p className="b mt0">{plan.get('description')}</p>
 
               	<ul>
                   {(plan.get('items') || []).map((item, index) => <li key={index}>
-                    <p className={index + 1 !== plan.get('items').size ? "pb2 mb2 divider-grey" : null}>{item}</p>
+                    <p className={index + 1 !== plan.get('items').size ? "pb2 mt0 mb2 divider-grey" : null}>{item}</p>
                   </li>)}
               	</ul>
 
